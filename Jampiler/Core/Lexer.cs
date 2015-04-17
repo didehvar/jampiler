e1b4a4d21e87563c5173e98fe25545abc75ca237
james <james@didehvar.com>
2015-04-17 16:58:02 +0100

    Make lexer a class, start implementing functions

---
+++ Jampiler/Core/Lexer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Jampiler.Core
+{
+    public class Lexer
+    {
+        private Regex _lineEnd = new Regex(@"\r\n|\r|\n");
+        private IList<TokenDefinition> _tokenDefinitions = new List<TokenDefinition>(); 
+
+        void AddDefinition(TokenDefinition definition)
+        {
+            _tokenDefinitions.Add(definition);
+        }
+
+        IEnumerable<Token> Tokenize(string source)
+        {
+            int currentIndex = 0;
+            int currentLine = 1;
+            int currentColumn = 0;
+
+            yield return new Token();
+        }
+    }
+}
