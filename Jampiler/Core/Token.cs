11bb04cabeda7986abfc209d5e3642794a2c8d0e
james <james@didehvar.com>
2015-04-17 16:35:14 +0100

    Simple token representation

---
+++ Jampiler/Core/Token.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Jampiler.Core
+{
+    public struct Token
+    {
+        public Regex Regex { get; set; }
+        public string Type { get; set; }
+    }
+}
