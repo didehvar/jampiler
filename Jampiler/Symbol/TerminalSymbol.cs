ea372ddc1919ed4bf986bea3f2aac790076783fb
james <james@didehvar.com>
2015-04-17 19:35:04 +0100

    Basic class for terminal symbols

---
+++ Jampiler/Symbol/TerminalSymbol.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jampiler.Symbol
+{
+    public class TerminalSymbol : Symbol
+    {
+        public string Value { get; set; }
+
+        public TerminalSymbol(string value)
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+}
