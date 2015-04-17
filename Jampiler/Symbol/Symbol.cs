0c1f47aa7f020cebe0e23640caa475809bdec547
james <james@didehvar.com>
2015-04-17 19:19:05 +0100

    Add symbol namespace, move Symbol there

--- Jampiler/Symbol/Symbol.cs
+++ Jampiler/Symbol/Symbol.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Jampiler.Core
+namespace Jampiler.Symbol
 {
     public abstract class Symbol
     {
