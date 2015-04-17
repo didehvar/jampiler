67f794a9deb0c8e73d34866e3326a4285ef83fdc
james <james@didehvar.com>
2015-04-17 18:59:39 +0100

    Basic lexer

--- Jampiler/Core/Lexer.cs
+++ Jampiler/Core/Lexer.cs
@@ -9,21 +9,67 @@ namespace Jampiler.Core
 {
     public class Lexer
     {
-        private Regex _lineEnd = new Regex(@"\r\n|\r|\n");
-        private IList<TokenDefinition> _tokenDefinitions = new List<TokenDefinition>(); 
+        private readonly Regex _lineEnd = new Regex(@"\r\n|\r|\n");
+        private readonly IList<TokenDefinition> _tokenDefinitions = new List<TokenDefinition>(); 
 
-        void AddDefinition(TokenDefinition definition)
+        public void AddDefinition(TokenDefinition definition)
         {
             _tokenDefinitions.Add(definition);
         }
 
-        IEnumerable<Token> Tokenize(string source)
+        public IEnumerable<Token> Tokenize(string source)
         {
-            int currentIndex = 0;
-            int currentLine = 1;
-            int currentColumn = 0;
+            var currentIndex = 0;
+            var currentLine = 1;
+            var currentColumn = 0;
 
-            yield return new Token();
+            while (currentIndex < source.Length)
+            {
+                TokenDefinition matchedDefinition = null;
+                var matchLength = 0;
+
+                foreach (var rule in _tokenDefinitions)
+                {
+                    var match = rule.Regex.Match(source, currentIndex);
+
+                    if (!match.Success /*|| (match.Index - currentIndex) != 0*/)
+                        continue;
+
+                    matchedDefinition = rule;
+                    matchLength = match.Length;
+
+                    break;
+                }
+
+                if (matchedDefinition == null)
+                {
+                    throw new Exception(string.Format("Unrecognised input :> {0} <: at {1} (line: {2}:{3})", source[currentIndex], currentIndex, currentLine, currentColumn));
+                }
+                else
+                {
+                    var value = source.Substring(currentIndex, matchLength);
+
+                    if (!matchedDefinition.Ignore)
+                    {
+                        yield return new Token(matchedDefinition.Type, value, new TokenPosition(currentIndex, currentLine, currentColumn));
+                    }
+
+                    var lineEndMatch = _lineEnd.Match(value);
+                    if (lineEndMatch.Success)
+                    {
+                        currentLine++;
+                        currentColumn = value.Length - (lineEndMatch.Index + lineEndMatch.Length);
+                    }
+                    else
+                    {
+                        currentColumn += matchLength;
+                    }
+
+                    currentIndex += matchLength;
+                }
+            }
+
+            yield return new Token("EoF", null, new TokenPosition(currentIndex, currentLine, currentColumn));
         }
     }
 }
--- Jampiler/Program.cs
+++ Jampiler/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Jampiler
@@ -11,6 +12,22 @@ namespace Jampiler
     {
         static void Main(string[] args)
         {
+            var lexer = new Lexer();
+
+            lexer.AddDefinition(new TokenDefinition("operator", new Regex(@"^\+|-|\*|\/|<|>|>=|<=|==|!=|and|or$", RegexOptions.IgnoreCase)));
+
+            var tokens = lexer.Tokenize(Console.ReadLine());
+
+            foreach (var token in tokens)
+            {
+                Console.WriteLine(token.Value);
+                Console.WriteLine(token.Type);
+                Console.WriteLine(token.Position.Index);
+                Console.WriteLine(token.Position.Column);
+                Console.WriteLine(token.Position.Line);
+            }
+
+            Console.ReadLine();
         }
     }
 }
