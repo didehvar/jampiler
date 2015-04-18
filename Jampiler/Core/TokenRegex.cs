8337eac76ef569bb9a40d5fe5e40d119692effb8
james <james@didehvar.com>
2015-04-18 22:34:20 +0100

    Fix identifier regex
    Allow one letter identifiers

--- Jampiler/Core/TokenRegex.cs
+++ Jampiler/Core/TokenRegex.cs
@@ -31,7 +31,7 @@ namespace Jampiler.Core
             Regexes.Add(TokenType.True, new Regex(@"true|TRUE"));
             Regexes.Add(TokenType.Local, new Regex(@"local"));
 
-            Regexes.Add(TokenType.Identifier, new Regex(@"[a-zA-Z_]\w+")); // Must come after keywords
+            Regexes.Add(TokenType.Identifier, new Regex(@"[a-zA-Z_]\w*")); // Must come after keywords
         }
 
         public static TokenRegex Instance => _instance ?? (_instance = new TokenRegex());
