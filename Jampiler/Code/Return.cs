05b4bb39e10b5812ab2e7567e43a2f74dfca93bb
james <james@didehvar.com>
2015-04-21 15:48:50 +0100

    Basic return code gen class

---
+++ Jampiler/Code/Return.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Jampiler.Code
+{
+    public class Return
+    {
+        public Data Data { get; set; }
+
+        public string Value { get; set; }
+
+        public string Text()
+        {
+            if (Data != null)
+            {
+                return string.Format("\tldr {0}, addr_{1}\n" + "\tldr, lr [{0}]", Data.Name, Data.Register);
+            }
+
+            if (!string.IsNullOrEmpty(Value))
+            {
+                return string.Format("\tmov r0, {0}", Value); ;
+            }
+
+            throw new Exception("Cannot convert empty return");
+        }
+    }
+}
